0381aca089f501c3db447cc499422c2ceb8bfe44
cmtucker <carolinemtucker@gmail.com>
2017-05-26 14:54:20 +0200

    set up initial conditions and algorithms. woohoo!

--- ExampleSimulationNullDevCalc.R
+++ ExampleSimulationNullDevCalc.R
@@ -12,24 +12,53 @@ library(reldist)
 library(vegan)
 library(bipartite)
 source("MetacommunityDynamicsFctsOikos.r")
-source("PANullDevFctsOikos.r")
 
-## Set metacommunity size, time length
-patches <- 25  # Number of patches
+## Set number of patches, species, time
+patches <- 10  # Number of patches
 species <- 25  # Number of species
 time <- 150	   # Length of model run (generations)
+initpop <- 25 #initial population size
 
+composition <- array(NA, dim=c(patches, species, time), dimnames=list(paste0("patches", 1:patches), paste0("species", 1:species), paste0("time", 1:time))) #where values = N
+composition[,,"time1"] <- initpop #N
 
-## Set parameters for metacommunity dynamics
-Rbest <- rep(NA, species)	# intrinsic growth-rate patch type 2 (optimal patch)
-d <- rep(0.05, species)		# migration rate
+#create env obj
+env <- 1:patches
+names(env) <- dimnames(composition)[[1]]
 
-Rbest <- rep(1.45, species) # Species sorting parameterization
+#create trait obj
+traits <- seq(from=1, to=patches, length.out=species)
+names(traits) <- dimnames(composition)[[2]]
 
-a <- 1/600					# Beverton-holt alpha for all species. This is fixed.
+#Calculate dist trait 
+disttraits <- as.matrix(dist(traits))
 
-R <- matrix(1.095833, nrow=species, ncol=patches) # intrinsic growth-rate patch type 1 (non-optimal patch)
-diag(R) <- Rbest
+#Calculate R 
+#calculates R given some env and trait combo
+envtrtcurve <- function(trti, envx, k=2, c=0.5){ k*exp(-1*((trti-envx)^2)/2*c^2)}
+
+#plot(envtrtcurve(1, env, 2, 0.5)~env, type="l")
+#points(envtrtcurve(6, env, 1, 0.5)~env, type="l")
+
+Rmatrix <- sapply(traits, envtrtcurve, envx=env)
+
+alphaterm <- function(distance, Nts){#needs pop sizes at time t
+	Nts %*% distance
+}
+
+alpha <- alphaterm(disttraits, composition[,,1])
+
+A = 0.001
+
+bevHoltFct <- function(R, N, A, alpha){ ((R * N) * 1)/(1 + A * alpha)
+	}
+
+for(m in seq(1, time - 1)){		
+	composition[,,m+1] <- bevHoltFct(Rmatrix, composition[,,m], A, alpha)	
+}
+
+plot(1:time, composition[5,13,], type="l")
+plot(1:time, composition[5,1,], type="l")
 
 # Data array for storing all results
 dat <- array(NA, c(species, patches, time))
