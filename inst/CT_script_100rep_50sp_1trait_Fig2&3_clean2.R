52323fa80ad747013097681de727146612ae88ff
Rekyt <matthias.grenie@ens-lyon.fr>
2020-07-29 17:53:09 +0200

    More comments & reproduce figure on ggplot2

--- inst/CT_script_100rep_50sp_1trait_Fig2&3_clean2.R
+++ inst/CT_script_100rep_50sp_1trait_Fig2&3_clean2.R
@@ -46,9 +46,13 @@ trait_scenar <- data.frame(trait            = "trait1",
 uncor_traits <- generate_cor_traits(n_patches, n_species, n_traits - 1,
                                     cor_coef = 0)
 uncor_traits <- uncor_traits[, 1, drop = FALSE]
-trait_comb <- seq(1:100)
 
-# Generate truly random trait distributions (trait 1 varies)
+# Number of combination of traits
+trait_comb <- seq(100)
+
+
+# Generate truly random trait distributions (trait 1 varies, instead of being
+# similar for all sets)
 for (i in trait_comb[-1]) {
     uncor_traits2 <- generate_cor_traits_rand(n_patches, n_species,
                                               n_traits - 1, cor_coef = 0)
@@ -69,11 +73,12 @@ comb <- distinct(comb) # remove duplicates
 
 
 # Computation & Outputs --------------------------------------------------------
-eq <- c()
-mis_i <- list()
-tra_env <- list()
+eq <- c()          # list of
+mis_i <- list()    # list of data.frame with species performance & patch of best
+                   # performance
+tra_env <- list()  # Contain CWM by environment
 
-# growth rates only estimated for final time step
+# total generation
 j = 2*n_gen
 
 for (i in 1:nrow(comb)) {
@@ -89,10 +94,11 @@ for (i in 1:nrow(comb)) {
         width = rep(width, n_patches), h_fun = "+", di_thresh = 24, K = 100)
 
 
-    ## Data frame of last time step with traits
+    ## Data frame of site vs. abundance at last time step
     tra_env_j <- reshape2::melt(simul_i$compo[, , j])
     colnames(tra_env_j) <- c("site", "sp", "ab")
 
+    ## Species trait data.frame
     sp_tra <- data.frame(sp = rownames(uncor_traits),
                          tra = uncor_traits[, comb[i, "trait_comb"]])
     tra_env_j <- suppressWarnings(left_join(tra_env_j, sp_tra, by = "sp"))
@@ -109,10 +115,11 @@ for (i in 1:nrow(comb)) {
 
     ## Compute growth rates
     max_time <- n_gen
-    gw <- extract_growth_rates(simul=simul_i, chosen_time = j)
+    gw <- extract_growth_rates(simul = simul_i, chosen_time = j)
 
     # Merge growth rates with the CWM data frame
-    tra_env_j <- left_join(tra_env_j, gw, by = c("env" = "patch", "sp" = "species"))
+    tra_env_j <- left_join(tra_env_j, gw,
+                           by = c("env" = "patch", "sp" = "species"))
 
     # Add trait weighted by the different growth rates per site
     # (scaling before)
@@ -124,9 +131,9 @@ for (i in 1:nrow(comb)) {
                                                  c(0, 1)),
                int_growth_rate = scales::rescale(int_growth_rate,
                                                  c(0, 1))) %>%
-        mutate(w_avg_gw = wtd_mean(tra, w = avg_growth_rate, na.rm=TRUE),
-               w_max_gw = wtd_mean(tra, w = max_growth_rate, na.rm=TRUE),
-               w_int_gw = wtd_mean(tra, w = int_growth_rate, na.rm=TRUE)) %>%
+        mutate(w_avg_gw = wtd_mean(tra, w = avg_growth_rate, na.rm = TRUE),
+               w_max_gw = wtd_mean(tra, w = max_growth_rate, na.rm = TRUE),
+               w_int_gw = wtd_mean(tra, w = int_growth_rate, na.rm = TRUE)) %>%
         distinct(site, .keep_all = TRUE) %>%
         select(-sp, -final_abundance, -avg_growth_rate,
                -max_growth_rate, -int_growth_rate) %>%
@@ -135,12 +142,15 @@ for (i in 1:nrow(comb)) {
     ## Bind patch level results
     tra_env[[i]] <- tra_env_j
 
-    ## Species mismatches
+    ## Compute Species Performance and Patch of Best performance for all species
+    # return information with growth from env. filtering only, growth from env.
+    # filtering and hierarchical competition, real abundance, and abundance
+    # based on environmental filtering only
     all_growth <- r_env_CT(simul_i, sp1 = n_species, n_patches = n_patches,
                            time = 50)
 
-    ## species level mismatches
-    matches <- matrix(NA, ncol=17, nrow=n_species)
+    ## Compute Species-level mismatches
+    matches <- matrix(NA, ncol = 17, nrow = n_species)
     for (z in 1:n_species){
 
         # Extract max performance and environmental of max performance for each
@@ -148,7 +158,7 @@ for (i in 1:nrow(comb)) {
         allout <- extract_mismatchesCT(x = all_growth, z = z)
 
         sub <- na.omit(gw[which(gw$sp == paste("species", z, sep="")), ])
-        if(nrow(sub)>0){
+        if (nrow(sub) > 0) {
             patch_finalabund <- sub[which.max(sub$final_abundance), "patch"]
             patch_avg_growth_rate <- sub[which.max(sub$avg_growth_rate), "patch"]
             patch_max_growth_rate <- sub[which.max(sub$max_growth_rate), "patch"]
@@ -164,7 +174,6 @@ for (i in 1:nrow(comb)) {
                              patch_max_growth_rate, max_growth_rate,
                              patch_int_growth_rate, int_growth_rate)
         } else {
-
             matches[z,] <- c(z, allout, NA, NA, NA, NA, NA, NA, NA, NA)
         }
     }
@@ -182,46 +191,47 @@ for (i in 1:nrow(comb)) {
     cat(100*i/nrow(comb), "\t")
 }
 
-#Save files
-#tra_env100 <- tra_env
-#mis_i100 <- mis_i
-
-
 # Mismatches by Species --------------------------------------------------------
 
+# Calculate all relative species-level mismatches
+# (either by performance or by patch)
 allmis <- bind_rows(mis_i)
 
-# Calculate all the various mismatches. Also super messy.
 
-# Final abundance is real abundance vs abundance considering environmental
-# filtering only (MisAbP)
-# prop change in obs ab from TRUE
-allmis$MisAbP <- 100*(allmis$finalabund - allmis$ObsAb)/n_patches
-#by patch match
-allmis$MisAbPatchP <- 100*(allmis$finalabundPatch - allmis$ObsAbPatch)/n_patches
+allmis = allmis %>%
+    mutate(
+        ## Real final abudance vs. abundance with only environmental filtering
+        MisAbP = finalabund - ObsAb,
+        # Same but using patch of greatest abundance
+        MisAbPatchP = finalabundPatch - ObsAbPatch,
+        ## Real intrinsic growth rate (growth rate at first timesteps) vs.
+        ## growth rate observed with environmental filtering only
+        MisinstRP = intGR - ObsR,
+        # Same but with patch of best growth
+        MisinstRPatchP = intGRPatch - ObsRPatch,
+        ## Average growth rate over all timesteps vs. growth rate observed with
+        ## environmental filtering only
+        MisavgGRP = avgGR - ObsR,
+        # Same but with patch of best growth
+        MisavgGRPatchP = avgGRPatch - ObsRPatch,
+        ## Maximum growth rate over all timesteps vs. growth rate observed with
+        ## environmental filtering only
+        MismaxGRP = maxGR - ObsR,
+        # Same but with patch of best growth
+        MismaxGRPatchP = maxGRPatch - ObsRPatch,
+        ## Concatenate parameter values
+        comb = paste(k, A, H, sep = "_")
+    ) %>%
+    # Make all mismatches relative to gradient length
+    mutate_at(vars(starts_with("Mis")), ~./n_patches)
 
-# Obs inst GR as measure
-# prop change in obs instR patch
-allmis$MisinstRP <- 100*(allmis$intGR - allmis$ObsR)/n_patches
-# patch match
-allmis$MisinstRPatchP <- 100*(allmis$intGRPatch - allmis$ObsRPatch)/n_patches
-
-# Avg GR as measure
-allmis$MisavgGRP <- 100*(allmis$avgGR - allmis$ObsR)/n_patches #prop change in obs instR
-allmis$MisavgGRPatchP <- 100*(allmis$avgGRPatch - allmis$ObsRPatch)/n_patches
-
-# Max GT as a measure
-allmis$MismaxGRP <- 100*(allmis$maxGR - allmis$ObsR)/n_patches
-allmis$MismaxGRPatchP <- 100*(allmis$maxGRPatch - allmis$ObsRPatch)/n_patches
-
-allmis$comb <- paste(allmis$k, allmis$A, allmis$H, sep="_")
-
- #ignore warnings
-allmisA <- aggregate(allmis, by=list(allmis$Species, allmis$comb), "mean", na.rm=TRUE)
+# Get average of mismatches per species
+allmisA <- aggregate(allmis, by = list(allmis$Species, allmis$comb), "mean",
+                     na.rm = TRUE)
 allmisA$comb <- allmisA$Group.2
 
 unique(allmisA$comb)
-#subset into distinct scenarios
+# subset into distinct scenarios
 nocomp <- allmisA[which(allmisA$comb %in% c("2_0_0")), ]
 Acomp <- allmisA[which(allmisA$comb %in% c("2_0.001_0")), ]
 Hcomp <- allmisA[which(allmisA$comb %in% c("2_0_0.001")), ]
@@ -230,40 +240,64 @@ allcomp <- allmisA[which(allmisA$comb %in% c("2_0.001_0.001")), ]
 
 # Plots of Mismatches by Species -----------------------------------------------
 ##mismatch plots, by species
-par(mfrow=c(1,2))
-plot(nocomp$Species~ nocomp$MisAbPatchP, cex=0.3, xlim=c(-25,25),
-     xlab="Mismatch from true topt (% of gradient)", ylab="Species",
-     main="+Limiting similarity", col="grey", type="l")
-abline(h=c(1:50), col="grey", lwd=0.2)
-points(Acomp$Group.1~Acomp$MisAbPatchP, cex=0.75, col="red")
-points((Acomp$Group.1) ~ (Acomp$MisinstRPatchP), cex=0.75, pch=16, col="orange")
-arrows(x0= Acomp$MisAbPatchP, x1=Acomp$MisinstRPatchP, y0=seq(1:50), y1=seq(1:50),code=0)
-points((Acomp$Species) ~ (Acomp$MismaxGRPatchP), cex=0.75, pch=09, col="blue")
-points((Acomp$Species) ~ (Acomp$MisavgGRPatchP), cex=0.75, pch=4, col="purple")
-arrows(x0= Acomp$MismaxGRPatchP, x1=Acomp$MisavgGRPatchP, y0=seq(1:50), y1=seq(1:50),code=0)
-arrows(x0= Acomp$MisPatchAbP, x1=Acomp$MisavgGRPatchP, y0=seq(1:50), y1=seq(1:50),code=0)
-
-points(y=c(50), x=10,  col="red")
-text(y=c(50), x=20, cex=0.75,"Obs abundance")
-points(y=c(48), x=10, pch=16, col="orange")
-text(y=c(48), x=20, cex=0.75,"Inst. growth rate")
-points(y=c(46), x=10,  pch=9, col="blue")
-text(y=c(46), x=20, cex=0.75,"Max growth rate")
-points(y=c(44), x=10,  pch=4, col="purple")
-text(y=c(44), x=20, cex=0.75,"Avg growth rate")
-#
-
 # Try reproducing plot using ggplot2
-
-Acomp %>%
-    select(Species, MisAbPatchP, MisinstRPatchP, MismaxGRPatchP,
+all_minmax_mismatch = allmisA %>%
+    select(Species, comb, MisAbPatchP, MisinstRPatchP, MismaxGRPatchP,
            MisavgGRPatchP) %>%
+    rowwise() %>%
+    mutate(min_mismatch = min(MisAbPatchP, MisinstRPatchP, MismaxGRPatchP,
+                              MisavgGRPatchP),
+           max_mismatch = max(MisAbPatchP, MisinstRPatchP, MismaxGRPatchP,
+                              MisavgGRPatchP)) %>%
+    select(Species, min_mismatch, max_mismatch, comb)
+
+plot_species_mismatch = allmisA %>%
+    filter(comb != "2_0_0") %>%
+    select(Species, MisAbPatchP, MisinstRPatchP, MismaxGRPatchP,
+           MisavgGRPatchP, comb) %>%
     tidyr::gather("mismatch_name", "mismatch_value",
                   MisAbPatchP:MisavgGRPatchP) %>%
+    inner_join(all_minmax_mismatch, by = c("Species", "comb")) %>%
+    mutate(comb = factor(comb, levels = c("2_0.001_0", "2_0_0.001",
+                                          "2_0.001_0.001"))) %>%
     ggplot(aes(mismatch_value, Species, color = mismatch_name,
                shape = mismatch_name)) +
-    geom_point() +
-    theme_bw()
+    geom_segment(aes(x = min_mismatch, xend = max_mismatch,
+                     yend = Species),
+                 color = "grey", size = 2/3) +
+    geom_vline(xintercept = 0, linetype = 1, size = 1/2, color = "black") +
+    geom_point(size = 1.5) +
+    facet_wrap(vars(comb), ncol = 3,
+               labeller = labeller(
+                   comb = c("2_0.001_0" = "+Limiting Similarity",
+                            "2_0_0.001" = "+Hierarchical Competition",
+                            "2_0.001_0.001" = "+Limiting Similarity\n+Hierarchical Competition"))) +
+    scale_x_continuous(labels = scales::label_percent()) +
+    scale_y_continuous(limits = c(1, 50), breaks = c(1, c(1,2,3,4,5)*10)) +
+    scale_color_discrete(labels = c(
+        MisAbPatchP = "Abundance",
+        MisavgGRPatchP = "Average Growth Rate",
+        MisinstRPatchP = "Intrinsic Growth Rate",
+        MismaxGRPatchP = "Maximum Growth Rate"
+    )) +
+    scale_shape_discrete(labels = c(
+        MisAbPatchP = "Abundance",
+        MisavgGRPatchP = "Average Growth Rate",
+        MisinstRPatchP = "Intrinsic Growth Rate",
+        MismaxGRPatchP = "Maximum Growth Rate"
+    )) +
+    labs(x = "Relative Mismatch from true optimal patch (% of gradient)",
+         shape = "Performance Measure",
+         color = "Performance Measure") +
+    theme_bw(12) +
+    theme(aspect.ratio = 1,
+          panel.grid.major.x = element_line(size = 1.3),
+          panel.spacing.x = unit(4, "mm"),
+          strip.background = element_blank(),
+          panel.border = element_blank(),
+          legend.position = "top")
+
+plot_species_mismatch
 
 #H competition
 plot(nocomp$Species~ nocomp$MisAbPatchP, cex=0.3, xlim=c(-25,25), xlab="Mismatch from true topt (% of gradient)", ylab="Species", main="+Hierarchical competition", type="l", col="grey")
