8f53b23ccd45d581b99409480bef326fcdb3b2e2
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2020-11-26 14:52:02 +0100

    Remove all unused scripts (cluster files, etc.)

--- inst/other_simuls_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 13)
-do
-    echo "Begin $i"
-    /c/Users/grenie/Documents/R/R-3.5.2/bin/Rscript.exe --arch x64 --vanilla inst/other_simuls.R $i > other_simuls_20190404_$i.out
-    wait
-    echo "End $i"
-done
