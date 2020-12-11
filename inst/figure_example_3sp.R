8af8efa2928acd0bfdd93197126f7b55faa40e1b
pierredenelle <pierre.denelle@gmail.com>
2020-12-11 11:06:10 +0100

    Figure with 3 species improved

--- inst/figure_example_3sp.R
+++ inst/figure_example_3sp.R
@@ -2,6 +2,7 @@
 library("dplyr")
 library("ggplot2")
 library("cowplot")
+library("ggrepel")
 library("tidyr")
 library("gridExtra")
 
@@ -28,7 +29,7 @@ composition <- array(NA, dim = c(n_patches, n_species, n_gen*20),
                                      paste0("time", seq(n_gen*20))))
 composition[, , 1] <- init_pop
 # trait values
-uncor_traits <- data.frame(trait1 = c(4, 4.1, 7))
+uncor_traits <- data.frame(trait1 = c(5, 5.1, 8))
 rownames(uncor_traits) <- paste0("species", seq(1:3))
 
 # parameter values: k not varying through the three examples
@@ -71,15 +72,26 @@ for(i in 1:length(three_scenarios)){
     simul_list[[i]] <- simul
 }
 
-## Plot -----------------------------------------------------------------------
-# explicit legend (strong lim.sim (A = bla); strong Hierarch.)
-
-# plot_patch and environmental response curves
+## Plots ----------------------------------------------------------------------
+# Environmental response curves of the 3 species
 env_resp <- r_env(simul, n_patches = 25, sp = 3, plot = TRUE)
-env_resp_plot <- env_resp[[2]] +
+env_resp_plot <- ggplot(env_resp[[1]], aes(env, r_env)) +
+    geom_ribbon(aes(x = env, ymin = 0, ymax = r_env, fill = as.factor(sp)),
+             alpha = 0.2, show.legend = FALSE) +
+    geom_line(aes(color = as.factor(sp))) +
+    geom_label_repel(
+        size = 5, show.legend = FALSE, force = 2, nudge_y = 0.5,
+        aes(color = as.factor(sp),
+            label = ifelse(env == max_r_env, sp, NA)),
+        size = 3) +
     scale_color_brewer(palette = "Dark2") +
+    scale_fill_brewer(palette = "Dark2") +
+    labs(x = "Patch", y = expression("r"["env"]),
+         title = "Environmental response curve of species") +
+    theme_classic() +
     guides(color = FALSE) +
-    geom_vline(xintercept = 4, linetype = "dashed")
+    theme(panel.border = element_rect(fill = NA)) +
+    geom_segment(x = 5, xend = 5, y = 0, yend = 2, linetype = "dashed")
 
 # Trait table
 table_tra_theme <- ttheme_minimal(
@@ -95,48 +107,54 @@ rownames(sp_tra) <- paste("Sp.", seq(1:3))
 
 tra_tbl <- tableGrob(sp_tra, theme = table_tra_theme)
 
-
 # Species dynamics (no competition)
-plotk_3sp <- plot_patch(simul_list[[1]]$compo, patch = 4, time =  20,
+plotk_3sp <- plot_patch(simul_list[[1]]$compo, patch = 5, time =  20,
                         equilibrium = FALSE) +
     scale_color_brewer(palette = "Dark2") +
     scale_y_continuous(limits = c(0, 50)) +
     guides(color = FALSE) +
-    geom_label(aes(label = ifelse(time == 20,
-                                  gsub("species", "sp", as.character(sp)),
-                                  NA))) +
-    labs(title = paste0("Patch 4; No competition"))
+    geom_label_repel(
+        size = 5, show.legend = FALSE, force = 2, nudge_y = 0.5,
+        aes(color = as.factor(sp),
+            label = ifelse(time == 20, gsub("species", "sp", as.character(sp)),
+                           NA))) +
+    labs(title = paste0("Patch 5; No competition"))
 
 # Species dynamics (lim.sim.)
-plotA_3sp <- plot_patch(simul_list[[2]]$compo, patch = 4, time =  20,
+plotA_3sp <- plot_patch(simul_list[[2]]$compo, patch = 5, time =  20,
                         equilibrium = FALSE) +
     scale_color_brewer(palette = "Dark2") +
     scale_y_continuous(limits = c(0, 50)) +
     guides(color = FALSE) +
-    geom_label(aes(label = ifelse(time == 20,
-                                  gsub("species", "sp", as.character(sp)),
-                                  NA))) +
-    labs(title = paste0("Patch 4; Limiting similarity")) +
+    geom_label_repel(
+        size = 5, show.legend = FALSE, force = 2, nudge_y = 0.5,
+        aes(color = as.factor(sp),
+            label = ifelse(time == 20, gsub("species", "sp", as.character(sp)),
+                           NA))) +
+    labs(title = paste0("Patch 5; Limiting similarity")) +
     # \n(A=", three_scenarios[[2]]$list_A, ")")) +
     theme(axis.title.y = element_blank(),
           axis.text.y = element_blank(),
           axis.ticks.y = element_blank())
 
 # Species dynamics (hierarchical competition)
-plotH_3sp <- plot_patch(simul_list[[3]]$compo, patch = 4, time =  20,
+plotH_3sp <- plot_patch(simul_list[[3]]$compo, patch = 5, time =  20,
                         equilibrium = FALSE) +
     scale_color_brewer(palette = "Dark2") +
     scale_y_continuous(limits = c(0, 50)) +
     guides(color = FALSE) +
-    geom_label(aes(label = ifelse(time == 20,
-                                  gsub("species", "sp", as.character(sp)),
-                                  NA))) +
-    labs(title = paste0("Patch 4; Hierarchical competition")) +
+    geom_label_repel(
+        size = 5, show.legend = FALSE, force = 2, nudge_y = 0.5,
+        aes(color = as.factor(sp),
+            label = ifelse(time == 20, gsub("species", "sp", as.character(sp)),
+                           NA))) +
+    labs(title = paste0("Patch 5; Hierarchical competition")) +
     # (H=", three_scenarios[[3]]$list_H, ")")) +
     theme(axis.title.y = element_blank(),
           axis.text.y = element_blank(),
           axis.ticks.y = element_blank())
 
+## Combined plot --------------------------------------------------------------
 # All plots together
 paper_figure3spexample <-
     plot_grid(plot_grid(grid.arrange(tra_tbl, env_resp_plot, nrow = 1,
@@ -150,5 +168,4 @@ paper_figure3spexample
 ggsave2("inst/figures/paper_figure_3spexample.png", paper_figure3spexample,
         width = 18, height = 10,
         units = "cm", dpi = 300, scale = 1.5)
-
 
